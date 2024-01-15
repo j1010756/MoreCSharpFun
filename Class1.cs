529ca02493066385bc3c0c55e030484d5a18f2ee
j1010756 <106291794+j1010756@users.noreply.github.com>
2024-01-15 12:45:13 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class Class1
+    {
+        private string Language;
+
+        // constructor
+
+        public Class1()
+        {
+            Language = "EN";
+        }
+        public Class1(string temp)
+        {
+            Language = temp;    
+        }
+        public void PrintName(string n, int num, bool blah)
+        {
+            if (Language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + n + "!");
+            }
+            if (Language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + n + "!");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MoreCSharpFun;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Class1 ps = new Class1("EN");
+
+
+        string name = "";
+
+        System.Console.WriteLine("Please Enter Your Name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName();
+    }
+}
